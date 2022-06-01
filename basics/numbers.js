ddab23b596a74b5499400b35749874e3cd965f4b
SebbyBlack <sebbyvonblack@gmail.com>
2022-06-01 17:30:25 -0500

    Completed numbers section of tutorial

---
+++ basics/numbers.js
@@ -0,0 +1,26 @@
+let x = 11;
+let numOne = 5.5;
+let numTwo = 5.5 - -1;
+let numThree = x + 1 * 2;
+let numFour = (x+1)*2;
+
+console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
+console.log(numFour);
+
+
+let age = 25;
+let dogYears = age * 7;
+
+console.log("I am 25, in dog years that is " + dogYears);
+
+// Challenge
+// Create a program to calculate a percent on a test based on answers they got right over total questions
+
+let correctAnswers = 18;
+let totalQuestions = 20;
+
+let studentsGrade = (correctAnswers / totalQuestions) * 100 + "%";
+
+console.log(studentsGrade);
