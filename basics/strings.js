519930f05d2b0b28a95bbebd52dfdafdd160b45d
SebbyBlack <sebbyvonblack@gmail.com>
2022-06-01 16:41:12 -0500

    Finished strings portion of tutorial

---
+++ basics/strings.js
@@ -0,0 +1,14 @@
+let firstName = "Sebastian";
+let lastName = "Black";
+
+console.log(firstName + " " + lastName);
+
+// Challenge
+// Three variables : city, country, location
+
+let city = "Denison";
+let state = "Texas";
+let country = "United States";
+let location = city + " " + state + ", " + country;
+
+console.log(location);
