a19ab57da302d3d69dea6809d2ac09a8ea83dec7
SebbyBlack <sebbyvonblack@gmail.com>
2022-06-01 18:20:46 -0500

    Completed temp convert challenge

---
+++ basics/tempConverter.js
@@ -0,0 +1,10 @@
+// Challenge
+// Variables : far, cel, Kelvin
+// Calculate F to C, print value then convert F to K, print value
+
+let farenheit = 32;
+let celsius = (farenheit - 32) * 5 / 9;
+let kelvin = (farenheit - 32) * 5 / 9 + 273.15;
+
+console.log(celsius);
+console.log(kelvin);
