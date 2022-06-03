83357b5f12176a30fb395ce8b26caaa429067d00
SebbyBlack <sebbyvonblack@gmail.com>
2022-06-03 14:56:48 -0500

    Finish functions tutorial one

---
+++ js-functions/functionsOne.js
@@ -0,0 +1,34 @@
+// Parts of a function : Input, Code, Output
+
+
+// Create the function
+let greetUser = function () {
+    console.log("Hello [insert name here]");
+}
+
+// Invoke the function
+greetUser();
+
+
+
+// Square number function
+
+let squaredNum = function (x) {
+    let sQ = x * x;
+
+    console.log(sQ);
+}
+
+squaredNum(3);
+squaredNum(5);
+
+
+// Fahrenheit to Celsius conversion function
+
+let fahToCel = function (fah) {
+    let conversion = (32 - fah) * 5 / 9;
+    console.log(conversion);
+}
+
+fahToCel(32);
+fahToCel(43);
